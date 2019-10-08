b3bdf0f87baa17da54c9189e86e44d9a7c2598fd
Qian.G.j <18515995323@163.com>
2019-10-08 17:24:53 +0800

    init

---
+++ Example/Pods/Target Support Files/MoFangWidgets/MoFangWidgets-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MoFangWidgets : NSObject
+@end
+@implementation PodsDummy_MoFangWidgets
+@end
---
+++ Example/Pods/Target Support Files/Pods-MoFangWidgets_Tests/Pods-MoFangWidgets_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MoFangWidgets_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MoFangWidgets_Tests
+@end
